818085e910b1f926135ec819a7784684a76ce7eb
Ryan <ryan.o.mackey@gmail.com>
2016-07-27 10:02:53 -0600

    initial

--- car.js
+++ car.js
@@ -1,9 +1,28 @@
-
+'use strict';
 
 var Car = function (mpg) {
+  this.mpg = mpg;
+  this.gallons = 0;
+  this.distanceTraveled = 0;
+  this.tripLog = [];
 };
 
 Car.prototype.fill = function (gallons) {
+  this.gallons += gallons;
+};
+
+Car.prototype.drive = function(miles) {
+  this.gallons -= miles / this.mpg;
+  this.distanceTraveled += miles;
+  this.tripLog.push(miles + ' miles');
+};
+
+Car.prototype.odometer = function() {
+  return this.distanceTraveled;
+};
+
+Car.prototype.trips = function() {
+  return this.tripLog;
 };
 
 module.exports = Car;
